6085349aabdf61567da60a0689038c9fb3119d09
rodrigo <zezocaplays312@hotmail.com>
2022-11-04 13:16:51 +0000

    loopsss

---
+++ loop_/loop_exercico.py
@@ -0,0 +1,16 @@
+import time
+
+for i in range(1,9) : # Write a for loop that counts to five.
+    print(i, "mississippi")  # Body of the loop - print the loop iteration number and the word "Mississippi".
+    time.sleep(1)   # Body of the loop - use: time.sleep(1)
+
+print(
+"""
+------------------------------------------------|
+|              tu contaste                      |
+|             8 mississippi                     |
+|                  <3                           |
+|                                               |
+|-----------------------------------------------|                                                 
+""")
+# Write a print function with thssage.e final me
