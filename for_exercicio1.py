a7e4cb1e3fff152bf15179d854b1794c0275da22
rodrigo <zezocaplays312@hotmail.com>
2022-11-09 12:26:40 +0000

    for

--- for_exercicio1.py
+++ for_exercicio1.py
@@ -1,10 +1,15 @@
+word_without_vowels = ""
+
 user_word= input("escreve uma palavra: ")
 print(user_word)
 user_word= user_word.upper()
 print(user_word)
 
 for letra in user_word:
-    if letra == "A":
-        continue
-    print(letra)
-
+    if letra == "A": continue
+    elif letra == "R": continue
+    elif letra == "O": continue
+    elif letra == "G": continue
+    word_without_vowels += letra 
+    
+    print(word_without_vowels)
